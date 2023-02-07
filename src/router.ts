5c7ee543340603f8b37a9bdc54c62b035178e15d
ddoyediran <ddoyediran@gmail.com>
2023-02-07 12:43:46 +0100

    updated all

---
+++ src/router.ts
@@ -0,0 +1,30 @@
+import { Router } from "express";
+
+const router = Router();
+
+/**
+ * PRODUCT
+ */
+router.get("/product", () => {});
+router.get("/product/:id", () => {});
+router.put("/product/:id", () => {});
+router.delete("/product/:id", () => {});
+router.post("/product", () => {});
+
+/**
+ * UPDATE
+ */
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.delete("/update/:id", () => {});
+router.post("/update", () => {});
+
+/**
+ * UPDATE POINT
+ */
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.delete("/updatepoint/:id", () => {});
+router.post("/updatepoint", () => {});
