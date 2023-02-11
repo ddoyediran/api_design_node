4ab2d3956e65c2359d23274b6606e14f5d28c53c
ddoyediran <ddoyediran@gmail.com>
2023-02-11 23:17:01 +0100

    jwt setup

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import { nextTick } from "process";
 
 export const createJWT = (user) => {
   const token = jwt.sign(
@@ -9,11 +10,28 @@ export const createJWT = (user) => {
   return token;
 };
 
-export const protect = (req, res) => {
+export const protect = (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer) {
     res.status(401).json({ message: "not authorized" });
+    return;
+  }
+
+  const [, token] = bearer.split(" ");
+
+  if (!token) {
+    res.status(401).json({ message: "invalid token" });
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "not authorized g" });
     return;
   }
 };
