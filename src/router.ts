95a5a90eb3a0c271f12781eafbc83122c8fef9f0
ddoyediran <ddoyediran@gmail.com>
2023-02-18 19:01:39 +0100

    update router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
+// import handleInputErrors from "./modules/middleware";
 
 const router = Router();
 
@@ -10,26 +13,80 @@ router.get("/product", () => (req, res) => {
 });
 
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
+
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
+
 router.delete("/product/:id", () => {});
-router.post("/product", () => {});
+
+router.post(
+  "/product",
+  body("name").isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 
 /**
  * UPDATE
  */
 router.get("/update", () => {});
+
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
+
+router.put(
+  "/update/:id",
+  body("body").optional(),
+  body("title").optional(),
+  body("version").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400);
+      res.json({ error: errors.array() });
+    }
+  }
+);
+
 router.delete("/update/:id", () => {});
-router.post("/update", () => {});
+
+router.post(
+  "/update",
+  body("body").exists().isString(),
+  body("title").exists().isString(),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400);
+      res.json({ error: errors.array() });
+    }
+  }
+);
 
 /**
  * UPDATE POINT
  */
 router.get("/updatepoint", (req, res) => {});
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  () => {}
+);
 router.delete("/updatepoint/:id", () => {});
-router.post("/updatepoint", () => {});
+router.post(
+  "/updatepoint",
+  body("name").isString(),
+  body("description").isString(),
+  body("updateId").exists().isString(),
+  () => {}
+);
 
 export default router;
