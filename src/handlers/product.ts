b65b9929db2a2dd539696d42bd00946565da3b6c
ddoyediran <ddoyediran@gmail.com>
2023-02-22 22:20:45 +0100

    add product handler

---
+++ src/handlers/product.ts
@@ -0,0 +1,29 @@
+import prisma from "../db";
+
+// Get all products
+export const getProducts = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user.id,
+    },
+    include: {
+      product: true,
+    },
+  });
+
+  res.json({ data: user.product });
+};
+
+// Get a single product
+export const getOneProduct = async (req, res) => {
+  const id = req.params.id;
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+      belongsToId: req.user.id,
+    },
+  });
+
+  res.json({ data: product });
+};
