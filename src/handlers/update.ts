c96cf7cd490a4e50d9097b4c5c1585c3a81ebf9e
ddoyediran <ddoyediran@gmail.com>
2023-02-24 16:24:32 +0100

    create update handler

---
+++ src/handlers/update.ts
@@ -0,0 +1,49 @@
+import prisma from "../db";
+
+// Get all updates
+export const getUpdates = async (req, res) => {
+  const products = await prisma.product.findMany({
+    where: {
+      belongsToId: req.user.id,
+    },
+    include: {
+      updates: true,
+    },
+  });
+
+  const updates = products.reduce((allUpdates, product) => {
+    return [...allUpdates, ...product.updates];
+  }, []);
+
+  res.json({ data: updates });
+};
+
+// Get a specific update
+export const getOneUpdate = async (req, res) => {
+  const updates = await prisma.update.findUnique({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.json({ data: updates });
+};
+
+// Delete a specific update
+export const deleteUpdate = async (req, res) => {
+  const deleteUpdated = await prisma.update.delete({
+    where: {
+      //   id_belongsToId: {
+      //     id: req.params.id,
+      //     belongsToId: req.user.id,
+      //   },
+    },
+  });
+
+  res.json({ data: deleteUpdated });
+};
+
+// to create a new update
+const createUpdate = async (req, res) => {};
+
+export const updateUpdate = async (req, res) => {};
