85760a0bdf76700cfd6b1bc916636c23a0d66349
ddoyediran <ddoyediran@gmail.com>
2023-02-26 21:24:14 +0100

    error handling added

--- src/handlers/user.ts
+++ src/handlers/user.ts
@@ -1,18 +1,23 @@
 import prisma from "../db";
 import { comparePasswords, createJWT, hashPassword } from "../modules/auth";
 
-export const createNewUser = async (req, res) => {
-  const hash = await hashPassword(req.body.password);
+export const createNewUser = async (req, res, next) => {
+  try {
+    const hash = await hashPassword(req.body.password);
 
-  const user = await prisma.user.create({
-    data: {
-      username: req.body.username,
-      password: hash,
-    },
-  });
+    const user = await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: hash,
+      },
+    });
 
-  const token = createJWT(user);
-  res.json({ token });
+    const token = createJWT(user);
+    res.json({ token });
+  } catch (e) {
+    e.type = "input";
+    next(e);
+  }
 };
 
 export const signin = async (req, res) => {
