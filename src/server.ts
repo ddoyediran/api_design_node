872d644e1a56414dfc44ca9586118d2c362af34a
ddoyediran <ddoyediran@gmail.com>
2023-02-27 19:04:55 +0100

    more commit

--- src/server.ts
+++ src/server.ts
@@ -22,4 +22,14 @@ app.get("/", (req, res) => {
   res.json({ message: "hello" });
 });
 
+app.use((err, req, res, next) => {
+  if (err.status === "auth") {
+    res.status(401).json({ message: "unauthorized" });
+  } else if (err.status === "input") {
+    res.status(400).json({ message: "invalid input" });
+  } else {
+    res.status(500).json({ message: "oops that is on us!" });
+  }
+});
+
 export default app;
